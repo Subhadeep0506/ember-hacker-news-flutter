4c4997781db26eeb09b3c37f682f0869d279fd15
Subhadeep Mandal <63490884+Subhadeep0506@users.noreply.github.com>
2026-08-22 18:06:34 +0000

    Fixed splash screen

--- lib/presentation/previews/ember_preview.dart
+++ lib/presentation/previews/ember_preview.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widget_previews.dart';
 
 import '../../config/theme/app_theme.dart';
@@ -12,10 +13,18 @@ final class EmberPreview extends Preview {
     super.brightness,
   }) : super(theme: EmberPreview._theme);
 
-  static PreviewThemeData _theme() {
-    return PreviewThemeData(
-      materialLight: AppTheme.light(),
-      materialDark: AppTheme.dark(),
+  static PreviewThemeData _theme() => const _EmberPreviewThemeData();
+}
+
+final class _EmberPreviewThemeData extends PreviewThemeData {
+  const _EmberPreviewThemeData();
+
+  @override
+  Widget apply(BuildContext context, Widget child) {
+    final isDark = Theme.of(context).brightness == Brightness.dark;
+    return Theme(
+      data: isDark ? AppTheme.dark() : AppTheme.light(),
+      child: child,
     );
   }
 }
